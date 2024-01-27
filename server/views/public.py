49c1cabfc5b9df219ef464f6a0b7d51b90427151
Sharon218 <sharonshajan2017@gmail.com>
2024-01-27 10:56:52 +0530

    Adding new route

--- server/views/public.py
+++ server/views/public.py
@@ -6,3 +6,8 @@ public = Blueprint("public", __name__, url_prefix="/")
 @public.route("/")
 def index():
     return "Hello world"
+
+
+@public.route("/model", methods=["GET"])
+def model_in():
+    return "Hi"
