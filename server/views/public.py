2dafde98e998df8d637c97b682000ed871a1c117
Sharon218 <sharonshajan2017@gmail.com>
2024-01-27 23:20:00 +0530

    Adding newsdataapi

--- server/views/public.py
+++ server/views/public.py
@@ -1,6 +1,7 @@
 import os
 from flask import Blueprint, Flask, request, jsonify
 from ..model.model import encode_input_data, load_rnn_model
+from newsdataapi import NewsDataApiClient
 
 public = Blueprint("public", __name__, url_prefix="/")
 
@@ -8,18 +9,21 @@ public = Blueprint("public", __name__, url_prefix="/")
 model = load_rnn_model(os.path.join(os.getcwd() + "/server/model/saved_model.keras"))
 print("model loaded")
 
+news_api = NewsDataApiClient(apikey="pub_37199c2a29d903fcc6895ca7992577b14d45f")
+
+
 @public.route("/")
 def index():
     return "Hello world"
 
 
-@public.route("/model")
+@public.route("/model", methods=["POST"])
 def model_in():
     input_ = request.get_json()["text"]
-    if type(input_)!=list:
+    if type(input_) != list:
         input_ = [input_]
     input_processed = encode_input_data(input_)
-    
+
     out = model.predict(input_processed)
     print("Input Encoded")
     print(input_processed)
@@ -29,4 +33,17 @@ def model_in():
     out_con = int(out < 0.5)
 
     return jsonify({"trust_value": out_con})
-    # return "hi"
+
+
+@public.route("/fetch_news", methods=["GET"])
+def fetch_news():
+    response = news_api.news_api(q="tech")
+
+    for article in response["results"]:
+        input_ = article["description"]
+        if type(input_) != list:
+            input_ = [input_]
+        input_processed = encode_input_data(input_)
+        out = model.predict(input_processed)
+        article["cred"] = f"{out[0][0]*100}"
+    return response["results"]
